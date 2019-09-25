130da44e34a5afb19449206739586a2f076d92a0
Lyssette <54973497+ly-sweetie@users.noreply.github.com>
2019-09-25 07:30:52 -0700

    Initial commit

---
+++ Operacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+   
+    class Operacion
+    {
+        //declarar global
+         double Res;
+        //Metodos con respectiva operacion
+        public double Suma(Valores V)
+        {
+            Res = V.ValorA + V.ValorB;
+            return Res;
+        }
+
+        public double Resta(Valores V)
+        {
+            Res = V.ValorA - V.ValorB;
+            return Res;
+        }
+
+        public double Multiplicar(Valores V)
+        {
+            Res = V.ValorA * V.ValorB;
+            return Res;
+
+        }
+
+        public double Division(Valores V)
+        {
+            Res = V.ValorA / V.ValorB;
+            return Res;
+        }
+    }
+}
